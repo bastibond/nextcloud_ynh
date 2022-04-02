2cde6e4e576eec6982e68c6dd5ffd5ccb2a3e1de
bastian <33595779+bastibond@users.noreply.github.com>
2022-04-02 02:05:26 +0200

    Update and rename upgrade.21.sh to upgrade.23.sh

---
+++ scripts/upgrade.d/upgrade.23.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Last available nextcloud version
+next_version="23.0.3"
+
+# Nextcloud tarball checksum sha256
+nextcloud_source_sha256="9b378b81d750212050059d9cc390d58813c5fb0f5b031d96d33dff8af693b092"
